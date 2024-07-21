a3c0927f1dd9079f71138f5648c32a7b577b7671
Brian Wakhale <Brianweke1998@gmail.com>
2024-07-21 09:47:01 +0300

    Modified JS

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Create a new button element for removing the task
       const removeButton = document.createElement('button');
       removeButton.textContent = "Remove";
-      removeButton.className = 'remove-btn';
+      removeButton.className = 'emove-btn';
   
       // Assign an onclick event to the remove button
       removeButton.onclick = () => {
